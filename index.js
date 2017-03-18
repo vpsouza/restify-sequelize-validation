3625e4e5987d01a8ff27b9306403841bf6f75481
vsouza <vinicius.rj@gmail.com>
2017-03-18 06:42:13 -0300

    update readme

---
+++ index.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const restify = require('restify'),
+      modelUtils = require('./lib/model-utils'),
+      getTenant = require('./lib/get-tenant');
+
+/**
+ * Applies the Sequelize model validation to request on a Restify webserver
+ * based on the given model name. 
+ * @param {object} server
+ * @param {string} modelName
+ * @param {object} models
+ * @return {function}
+ */
+module.exports = function (server, modelName, models){
+
+    function createErrorResponseObj(resultError, modelName){
+        let errorResponse = {model: modelName};
+
+        resultError.errors.forEach((elm) => {
+            if(!errorResponse[elm.path]){
+                errorResponse[elm.path] = [];
+            }
+
+            errorResponse[elm.path].push({
+                type: elm.type,
+                message: elm.message
+            });
+        });
+
+        return errorResponse;
+    }
+
+    return function(req,res,next){
+        if(!req.body){
+            return res.send(new restify.BadRequestError("Request Body Empty!"));
+        } else {
+            let modelInstance = getModelInstance(server, getTenant(req), modelName, models);
+            modelInstance.build(req.body).validate().then(function(result){
+                if(result && result.errors && result.errors.length > 0){
+                    return res.send(400, createErrorResponseObj(result, modelName));
+                }
+
+                let includedModels = modelUtils.getIncludedModels(req.body, modelInstance, true);
+                if(includedModels.length > 0){
+                    let arrayToBeValidated = [];
+                    includedModels.forEach(function(includedModel){
+                        arrayToBeValidated = arrayToBeValidated.concat(req.body[includedModel.relName].map(function(elm){
+                            return includedModel.target.build(elm);
+                        }));
+                    });
+
+                    Promise.all(arrayToBeValidated.map(function(elm){
+                        return elm.validate();
+                    })).then(
+                        function(result){
+                            if(result){
+                                let invalidItems = result.filter(function(elm){
+                                    return elm && elm.errors && elm.errors.length && elm.errors.length > 0;
+                                });
+                                if(invalidItems.length > 0){
+                                    let detailedInvalidItems = [];
+                                    result.forEach(function(elm, index){
+                                        if(elm.errors && elm.errors.length && elm.errors.length > 0){
+                                            detailedInvalidItems.push(createErrorResponseObj(elm, modelName + "." + arrayToBeValidated[index].$modelOptions.name.singular));
+                                        }
+                                    });
+
+                                    return res.send(400, detailedInvalidItems);
+                                }
+                                next();
+                            } else {
+                                next();
+                            }							
+                        }
+                    );
+                } else {
+                    next();
+                }
+            })
+        }
+    };
+
+    function getModelInstance(server, tenantID, modelName, models){
+        return models(server, tenantID)[modelName.charAt(0).toUpperCase() + modelName.slice(1)];
+    }
+}
---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+var expect = require('chai').expect;
+var validation = require('../index');
+
+describe('#sequelize-validation', function () {
+    it('should convert single digits', function () {
+        var result = '1';
+        expect(result).to.equal('1');
+    });
+});
